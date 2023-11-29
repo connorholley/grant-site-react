2d506f690984f226696caae2aaee5c53c7387aa7
connorholley <connorholley1@gmail.com>
2023-11-29 09:40:15 -0800

    fixed footer stuff

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -36,111 +36,113 @@ const Layout = () => {
 
   return (
     <>
-      <header className="bg-blue-500 font-sans">
-        <nav
-          className="flex items-center justify-between p-6 lg:px-8"
-          aria-label="Global"
-        >
-          <div className="flex lg:flex-1">
-            <div class="flex items-center flex-col">
-              <img
-                className="h-14  rounded-full border-orange-500 border-2"
-                src={require("../assets/logan.jpg")}
-                alt=""
-              />
-              <p class="text-center text-lg text-white">Grantable</p>
-            </div>
-          </div>
-          <div className="flex lg:hidden">
-            <button
-              type="button"
-              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-              onClick={() => setMobileMenuOpen(true)}
-            >
-              <span className="sr-only">Open main menu</span>
-              <Bars3Icon className="h-6 w-6" aria-hidden="true" />
-            </button>
-          </div>
-          <div className="hidden lg:flex lg:gap-x-12">
-            {navigation.map((item) => (
-              <Link
-                key={item.section}
-                to={item.href}
-                className={`text-sm font-semibold leading-6 text-gray-900 p-2 rounded-md  ${
-                  selectedSection === item.section
-                    ? "bg-orange-500   "
-                    : "bg-white"
-                }`}
-                onClick={() => handleSectionClick(item.section)}
-              >
-                {item.name}
-              </Link>
-            ))}
-          </div>
-          <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-            <a
-              href="login"
-              className="text-sm font-semibold leading-6 text-white"
-              onClick={() => handleSectionClick("none")}
-            >
-              Log in <span aria-hidden="true">&rarr;</span>
-            </a>
-          </div>
-        </nav>
-        <Dialog
-          as="div"
-          className="lg:hidden"
-          open={mobileMenuOpen}
-          onClose={setMobileMenuOpen}
-        >
-          <div className="fixed inset-0 z-10" />
-          <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-blue-500 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-            <div className="flex items-center justify-between">
-              <div className="flex lg:flex-1">
-                <div class="flex items-center flex-col">
-                  <img
-                    className="h-14  rounded-full border-orange-500 border-2"
-                    src={require("../assets/logan.jpg")}
-                    alt=""
-                  />
-                  <p class="text-center text-lg text-white">Grantable</p>
-                </div>
+      <div className="flex flex-col min-h-screen">
+        <header className="bg-blue-500 font-sans">
+          <nav
+            className="flex items-center justify-between p-6 lg:px-8"
+            aria-label="Global"
+          >
+            <div className="flex lg:flex-1">
+              <div class="flex items-center flex-col">
+                <img
+                  className="h-14  rounded-full border-orange-500 border-2"
+                  src={require("../assets/logan.jpg")}
+                  alt=""
+                />
+                <p class="text-center text-lg text-white">Grantable</p>
               </div>
+            </div>
+            <div className="flex lg:hidden">
               <button
                 type="button"
-                className="-m-2.5 rounded-md p-2.5 text-gray-700"
-                onClick={() => setMobileMenuOpen(false)}
+                className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+                onClick={() => setMobileMenuOpen(true)}
               >
-                <span className="sr-only">Close menu</span>
-                <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                <span className="sr-only">Open main menu</span>
+                <Bars3Icon className="h-6 w-6" aria-hidden="true" />
               </button>
             </div>
-            <div className="mt-6 flow-root">
-              <div className="-my-6 divide-y divide-gray-500/10">
-                <div className="space-y-2 py-6">
-                  {navigation.map((item) => (
-                    <Link
-                      key={item.section}
-                      to={item.href}
-                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                      onClick={() => {
-                        handleSectionClick(item.section);
-                        setMobileMenuOpen(false);
-                      }}
-                    >
-                      {item.name}
-                    </Link>
-                  ))}
+            <div className="hidden lg:flex lg:gap-x-12">
+              {navigation.map((item) => (
+                <Link
+                  key={item.section}
+                  to={item.href}
+                  className={`text-sm font-semibold leading-6 text-gray-900 p-2 rounded-md  ${
+                    selectedSection === item.section
+                      ? "bg-orange-500   "
+                      : "bg-white"
+                  }`}
+                  onClick={() => handleSectionClick(item.section)}
+                >
+                  {item.name}
+                </Link>
+              ))}
+            </div>
+            <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+              <a
+                href="login"
+                className="text-sm font-semibold leading-6 text-white"
+                onClick={() => handleSectionClick("none")}
+              >
+                Log in <span aria-hidden="true">&rarr;</span>
+              </a>
+            </div>
+          </nav>
+          <Dialog
+            as="div"
+            className="lg:hidden"
+            open={mobileMenuOpen}
+            onClose={setMobileMenuOpen}
+          >
+            <div className="fixed inset-0 z-10" />
+            <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-blue-500 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+              <div className="flex items-center justify-between">
+                <div className="flex lg:flex-1">
+                  <div class="flex items-center flex-col">
+                    <img
+                      className="h-14  rounded-full border-orange-500 border-2"
+                      src={require("../assets/logan.jpg")}
+                      alt=""
+                    />
+                    <p class="text-center text-lg text-white">Grantable</p>
+                  </div>
+                </div>
+                <button
+                  type="button"
+                  className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                  onClick={() => setMobileMenuOpen(false)}
+                >
+                  <span className="sr-only">Close menu</span>
+                  <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                </button>
+              </div>
+              <div className="mt-6 flow-root">
+                <div className="-my-6 divide-y divide-gray-500/10">
+                  <div className="space-y-2 py-6">
+                    {navigation.map((item) => (
+                      <Link
+                        key={item.section}
+                        to={item.href}
+                        className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                        onClick={() => {
+                          handleSectionClick(item.section);
+                          setMobileMenuOpen(false);
+                        }}
+                      >
+                        {item.name}
+                      </Link>
+                    ))}
+                  </div>
                 </div>
               </div>
-            </div>
-          </Dialog.Panel>
-        </Dialog>
-      </header>
-      <Outlet />
-      <footer className="bg-blue-500 p-4 text-white">
-        <p>Grantable | Connor Holley | ENSH 401</p>
-      </footer>
+            </Dialog.Panel>
+          </Dialog>
+        </header>
+        <Outlet />
+        <footer className="bg-blue-500 p-4 text-white bottom-0">
+          <p>Grantable | Connor Holley | ENSH 401</p>
+        </footer>
+      </div>
     </>
   );
 };
